e35e2ea91ec0645b2f2dcf8225c3040cfe670dcb
Dini Na <dna975@UoA.auckland.ac.nz>
2024-10-15 10:45:20 +1300

    install dependencies testng & rest assured

---
+++ APIAutomationTesting/src/Basics.java
@@ -0,0 +1,14 @@
+
+
+public class Basics {
+
+	public static void main(String[] args) {
+		//Validate if Add Place API is working as expected
+		// Given - All input details
+		// When - Submit the API
+		// Then - Validate the response
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
+
+	}
+
+}
