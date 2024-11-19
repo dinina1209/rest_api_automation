3c84e5ea3f841c50e7d26af1575aa8b32be3b47a
Dini Na <dna975@UoA.auckland.ac.nz>
2024-11-20 10:02:10 +1300

    setup cucumber BDD and step definition

--- APIFramework/src/test/java/stepDefinitions/StepDefinition.java
+++ APIFramework/src/test/java/stepDefinitions/StepDefinition.java
@@ -18,51 +18,25 @@ import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import pojo.AddPlace;
 import pojo.Location;
+import resources.TestDataBuild;
+import resources.Utils;
 
 
-public class StepDefinition {
+public class StepDefinition extends Utils{
 	ResponseSpecification resSpec;
 	RequestSpecification res;
 	Response response;
+	TestDataBuild data = new TestDataBuild();
 	
 	@Given("Add Place Payload")
 	public void add_Place_Payload() {
-		
-		//from SpecBuilderTest
-		RestAssured.baseURI = "https://rahulshettyacademy.com/";
-		
-		//creating place object using POJO
-		AddPlace place = new AddPlace();
-		place.setAccuracy(50);
-		place.setAddress("&quot;29, side layout, cohen 09");
-		place.setLanguage("French-IN");
-		place.setPhone_number("(+91) 983 893 3937");
-		place.setWebsite("http://google.com");
-		place.setName("Frontline house");
-		List<String> myList = new ArrayList<String>();
-		myList.add("shoe park");
-		myList.add("shop");
-		place.setTypes(myList);
-		Location loc = new Location();
-		loc.setLat(-38.383494);
-		loc.setLng(33.427362);
-		place.setLocation(loc);
-		
-		//RequestSpecification
-		RequestSpecification req = new RequestSpecBuilder()
-		.setBaseUri("https://rahulshettyacademy.com")
-		.addQueryParam("key", "qaclick123")
-		.setContentType(ContentType.JSON)
-		.build();
-		
-		//ResponseSpecification
 		resSpec = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
-		res = given().spec(req).body(place);
+		res = given().spec(requestSpecification()).body(data.addPlacePyaload());
 	}
 	
 	@When("user calls {string} with Post http request")
 	public void user_calls_with_Post_http_request(String string) {
-		response = res.when().post("/maps/api/place/add/json")
+		response = res.when().post("/maps/api/plac e/add/json")
 				.then().spec(resSpec).extract().response();
 	}
 	
