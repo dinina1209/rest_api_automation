74194be16bc43cb82c7cbd9aa130b6b018ebb5bc
Dini Na <dna975@UoA.auckland.ac.nz>
2024-11-20 14:01:33 +1300

    make baseURI as a globalVariable

--- APIFramework/src/test/java/resources/Utils.java
+++ APIFramework/src/test/java/resources/Utils.java
@@ -1,8 +1,10 @@
 package resources;
 
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Properties;
 
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
@@ -14,13 +16,12 @@ import io.restassured.specification.RequestSpecification;
 public class Utils {
 	RequestSpecification req;
 	
-	public RequestSpecification requestSpecification() throws FileNotFoundException {
+	public RequestSpecification requestSpecification() throws IOException {
 		
 		PrintStream log = new PrintStream(new FileOutputStream("logging.txt"));
-		RestAssured.baseURI = "https://rahulshettyacademy.com/";
 		
 		req = new RequestSpecBuilder()
-		.setBaseUri("https://rahulshettyacademy.com")
+		.setBaseUri(getGlobalValue("baseUrl"))
 		.addQueryParam("key", "qaclick123")
 		.addFilter(RequestLoggingFilter.logRequestTo(log))
 		.addFilter(ResponseLoggingFilter.logResponseTo(log))
@@ -28,5 +29,13 @@ public class Utils {
 		.build();
 		
 		return req;
+	}
+	
+	public static String getGlobalValue(String key) throws IOException
+	{
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream("C:\\Users\\dna975\\eclipse-workspace\\APIFramework\\src\\test\\java\\resources\\global.properties");
+		prop.load(fis);
+		return prop.getProperty(key);	
 	}
 }
